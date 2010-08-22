83d8450c20d5347cb30b238ebd0ce8d780a1e8ab
Jesse van den Kieboom <jessevdk@gnome.org>
2010-08-22 23:52:25 +0200

    Fix behavior of InfTextGtkHueChooser
    2010-08-22  Jesse van den Kieboom  <jessevdk@gnome.org>
    
    	* libinftextgtk/inf-text-gtk-hue-chooser.c
    	(inf_text_gtk_hue_chooser_realize): Set correct user_data for correct
    	window, fixing the behavior of the hue chooser.

--- libinftextgtk/inf-text-gtk-hue-chooser.c
+++ libinftextgtk/inf-text-gtk-hue-chooser.c
@@ -537,7 +537,7 @@ inf_text_gtk_hue_chooser_realize(GtkWidget* widget)
 #endif
 
   priv->window = gdk_window_new(parent_window, &attr, GDK_WA_X | GDK_WA_Y);
-  gdk_window_set_user_data(parent_window, chooser);
+  gdk_window_set_user_data(priv->window, chooser);
   gtk_widget_set_style(
     widget,
     gtk_style_attach(gtk_widget_get_style(widget), parent_window)
