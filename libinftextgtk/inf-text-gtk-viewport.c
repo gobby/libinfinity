69caa6a477322ee68e55bc5c3f38515c02428edd
Armin Burgmeier <armin@arbur.net>
2011-01-23 16:32:54 +0100

    Add 2011 to copyright years
    2011-01-23  Armin Burgmeier  <armin@arbur.net>
    
    	* all source files: Add 2011 to copyright years.

--- libinftextgtk/inf-text-gtk-viewport.c
+++ libinftextgtk/inf-text-gtk-viewport.c
@@ -1,5 +1,5 @@
 /* infinote - Collaborative notetaking application
- * Copyright (C) 2007-2010 Armin Burgmeier <armin@arbur.net>
+ * Copyright (C) 2007-2011 Armin Burgmeier <armin@arbur.net>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
