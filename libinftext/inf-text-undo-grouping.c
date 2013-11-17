68c9d3c6aad7f3ce106211253dc2e92b83e36e00
Armin Burgmeier <armin@arbur.net>
2013-11-17 15:39:20 +0100

    Use inf_adopted_request_get_index() in InfTextUndoGrouping
    It makes the code a bit more readable.

--- libinftext/inf-text-undo-grouping.c
+++ libinftext/inf-text-undo-grouping.c
@@ -97,7 +97,7 @@ inf_text_undo_grouping_get_translated_position(InfAdoptedAlgorithm* algorithm,
   inf_adopted_state_vector_set(
     move_vec,
     move_id,
-    inf_adopted_state_vector_get(inf_adopted_request_get_vector(to), move_id)
+    inf_adopted_request_get_index(to)
   );
   move_req = inf_adopted_request_new_do(
     move_vec,
