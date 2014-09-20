gobby/libinfinity

commit 37afb068e89fc1aed45f795581904c3aaec34e36

Fix a crash when a certificate is added into the known hosts file
Typically this crash only happened at the point when the connection that
presented that certificate was closed.

libinfgtk/inf-gtk-certificate-manager.c
@@ -579,7 +579,7 @@ inf_gtk_certificate_manager_response_cb(GtkDialog* dialog,
         error->message
       );
     }
-    else if(!cert_equal)
+    else if(known_cert != NULL && !cert_equal)
     {
       g_hash_table_insert(query->known_hosts, hostname, cert);
 

commit e43df07b37dc2a109cb476367173530e4b7917e9

Write certificate to known hosts file also if it was not present before
Regression caused by 37afb068e89fc1aed45f795581904c3aaec34e36.

libinfgtk/inf-gtk-certificate-manager.c
@@ -565,11 +565,6 @@ inf_gtk_certificate_manager_response_cb(GtkDialog* dialog,
         known_cert,
         &error
       );
-
-      if(error == NULL && !cert_equal)
-      {
-        cert = inf_cert_util_copy_certificate(cert, &error);
-      }
     }
 
     if(error != NULL)
@@ -579,8 +574,9 @@ inf_gtk_certificate_manager_response_cb(GtkDialog* dialog,
         error->message
       );
     }
-    else if(known_cert != NULL && !cert_equal)
+    else if(!cert_equal)
     {
+      cert = inf_cert_util_copy_certificate(cert, &error);
       g_hash_table_insert(query->known_hosts, hostname, cert);
 
       inf_gtk_certificate_manager_write_known_hosts_with_warning(