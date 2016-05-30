8eb9560de3ef5f0d7aa7f9f48ea9e3b55af24933
Armin Burgmeier <armin@arbur.net>
2016-05-30 13:22:52 -0700

    Fix warning in usage of gtk_label_set_xalign

--- libinfgtk/inf-gtk-certificate-manager.c
+++ libinfgtk/inf-gtk-certificate-manager.c
@@ -252,7 +252,7 @@ inf_gtk_certificate_manager_check_certificate(
   gtk_widget_set_halign(GTK_WIDGET(label), GTK_ALIGN_START);
   gtk_widget_set_valign(GTK_WIDGET(label), GTK_ALIGN_START);
 #if GTK_CHECK_VERSION(3,16,0)
-  gtk_label_set_xalign(GTK_WIDGET(label), 0.0);
+  gtk_label_set_xalign(GTK_LABEL(label), 0.0);
 #else
   gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.0);
 #endif
